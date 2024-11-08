242e20788e59587cc54ff0a371e00de5ceca7b7a
bgocean <bigocean700@gmail.com>
2024-11-08 03:02:42 +0200

    initial commit

--- gun.py
+++ gun.py
@@ -6,7 +6,7 @@ class Gun():
         """Инициализация пушки"""
 
         self.screen = screen
-        self.image = pygame.image.load('img/gun.png')
+        self.image = pygame.image.load('img/gun2.png')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
@@ -21,6 +21,6 @@ class Gun():
     def update_gun(self):
         """Обновление позиции пушки"""
         if self.move_right and self.rect.right < self.screen_rect.right:
-            self.rect.centerx += 1
+            self.rect.centerx += 2
         if self.move_left and self.rect.left > 0:
-            self.rect.centerx -= 1
+            self.rect.centerx -= 2
